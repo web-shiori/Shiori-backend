45c0344a821b355e61b34e6fc324ab776afe5cc3
masatora <atarashi.masatora@gmail.com>
2021-08-24 20:06:04 +0900

    Introduce devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   devise_for :users
   namespace :v1, {format: 'json'} do
     resources :content, only: %i[index create update destroy]
