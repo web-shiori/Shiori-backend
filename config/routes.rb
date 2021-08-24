ad82f4d48caf108a856c46f83f987fc841ed5495
masatora <atarashi.masatora@gmail.com>
2021-08-24 20:11:13 +0900

    usersテーブルにdevise_token_auth用のカラムを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  devise_for :users
   namespace :v1, {format: 'json'} do
     resources :content, only: %i[index create update destroy]
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, via: %i[get post]
   end
 end
--- db/migrate/20210824110553_devise_token_auth_create_users.rb
+++ db/migrate/20210824110553_devise_token_auth_create_users.rb
@@ -1,49 +1,26 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
-  def change
-    
-    create_table(:users) do |t|
-      ## Required
-      t.string :provider, :null => false, :default => "email"
-      t.string :uid, :null => false, :default => ""
+  def up
+    add_column :users, :provider, :string, null: false, default: 'email'
+    add_column :users, :uid, :string, null: false, default: ''
+    add_column :users, :tokens, :text
 
-      ## Database authenticatable
-      t.string :encrypted_password, :null => false, :default => ""
+    # if your existing User model does not have an existing **encrypted_password** column uncomment below line.
+    # add_column :users, :encrypted_password, :null => false, :default => ""
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-      t.boolean  :allow_password_change, :default => false
+    # the following will update your models so that when you run your migration
 
-      ## Rememberable
-      t.datetime :remember_created_at
+    # updates the user table immediately with the above defaults
+    User.reset_column_information
 
-      ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-      ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
-      t.string :email
-
-      ## Tokens
-      t.json :tokens
-
-      t.timestamps
+    # finds all existing users and updates them.
+    # if you change the default values above you'll also have to change them here below:
+    User.find_each do |user|
+      user.uid = user.email
+      user.provider = 'email'
+      user.save!
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, [:uid, :provider],     unique: true
-    add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    # to speed up lookups to these columns:
+    add_index :users, %i[uid provider], unique: true
   end
 end
