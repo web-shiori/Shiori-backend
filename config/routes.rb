aa56676a8017c847d638165840b80ba261e6a5f7
masatora <atarashi.masatora@gmail.com>
2021-08-24 19:45:08 +0900

    Add hello world API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1, {format: 'json'} do
+    resources :content, only: %i[index create update destroy]
+  end
 end
