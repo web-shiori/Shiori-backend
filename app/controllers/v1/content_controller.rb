7e5c913e9a64b7bfae789ca6538a62990b491619
masatora <atarashi.masatora@gmail.com>
2021-08-25 12:10:40 +0900

    コンテンツ一覧エンドポイントの基礎作成

--- app/controllers/v1/content_controller.rb
+++ app/controllers/v1/content_controller.rb
@@ -1,7 +1,12 @@
 class V1::ContentController < ApplicationController
   def index
-    render json: {
-        "hello": "world"
-    }
+    @content = Content.order('created_at DESC')
+                      # .where(user_id: current_user.id)
+                      # .search_by_keyword(query)
+                      # .page(page)
+                      # .per(params[per_page)
+    render json: @content
   end
+
+
 end
