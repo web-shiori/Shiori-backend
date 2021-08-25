b37f59674893b9b1cf5c3714c76621a727d84df4
masatora <atarashi.masatora@gmail.com>
2021-08-25 16:27:30 +0900

    死活監視用エンドポイント追加

--- app/controllers/v1/application_controller.rb
+++ app/controllers/v1/application_controller.rb
@@ -2,4 +2,8 @@ class V1::ApplicationController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session, if: -> { request.format.json? }
 
+  # 死活監視用エンドポイント
+  def heartbeat
+    render json: { "status": "alive" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'devise_token_auth'
 Rails.application.routes.draw do
+  get '/heartbeat' => 'v1/application#heartbeat'
   namespace :v1, {format: 'json'} do
     resources :content, only: %i[index create update destroy]
     mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, via: %i[get post]
