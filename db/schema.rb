c4680b65efe15af6feb0c3df6f348173baea546d
masatora <atarashi.masatora@gmail.com>
2021-08-25 19:53:13 +0900

    contentテーブルにlikedカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_104946) do
+ActiveRecord::Schema.define(version: 2021_08_25_105221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_104946) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "content_type"
+    t.boolean "liked", default: false
     t.index ["id", "user_id"], name: "index_content_on_id_and_user_id"
     t.index ["user_id"], name: "index_content_on_user_id"
   end
