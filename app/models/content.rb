85c24a170dd978d85b0e1be3095fe6095f09e84c
masatora <atarashi.masatora@gmail.com>
2021-08-25 12:10:30 +0900

    fix: contentモデルのバグ修正

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,9 +1,11 @@
 class Content < ApplicationRecord
+  self.table_name = "content"
+
   belongs_to :user
 
-  validates :user_id, presense: true
-  validates :title, presense: true, length: { maximum: 255 }
-  validates :url, presense: true
-  validates :sharing_url, presense: true
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :url, presence: true
+  validates :sharing_url, presence: true
 
 end
