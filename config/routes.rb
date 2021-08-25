4d17212ee4782d0b286f461e5ab3a5cc39cb014a
masatora <atarashi.masatora@gmail.com>
2021-08-25 19:35:30 +0900

    お気に入り関連のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/heartbeat' => 'v1/application#heartbeat'
   namespace :v1, {format: 'json'} do
     resources :content, only: %i[index create update destroy]
+    get '/likes' => 'likes#index'
+    post '/content/:content_id/like' => 'likes#create'
+    delete '/content/:content_id/like' => 'likes#delete'
     mount_devise_token_auth_for 'User', at: 'auth', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, via: %i[get post]
   end
 end
