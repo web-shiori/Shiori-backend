4e78ad863176b0e26d771726db5ffa57e4acb3ce
masatora <atarashi.masatora@gmail.com>
2021-08-25 12:39:58 +0900

    コンテンツ検索に対応

--- app/controllers/v1/content_controller.rb
+++ app/controllers/v1/content_controller.rb
@@ -1,8 +1,12 @@
 class V1::ContentController < ApplicationController
   def index
+    # @content = Content.order('created_at DESC')
+    #                   # .where(user_id: current_user.id)
+    #                   # .search_by_keyword(query)
+    #                   # .page(page)
+    #                   # .per(params[per_page)
     @content = Content.order('created_at DESC')
-                      # .where(user_id: current_user.id)
-                      # .search_by_keyword(query)
+                      .where('LOWER(title) LIKE ?', "%#{params[:q]}%")
                       # .page(page)
                       # .per(params[per_page)
     render json: @content
