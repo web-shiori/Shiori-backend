bd5060e97239891219c475c83e8c172a024d78fd
masatora <atarashi.masatora@gmail.com>
2021-08-25 16:33:55 +0900

    likesテーブルを作成

---
+++ db/migrate/20210825073117_create_likes.rb
@@ -0,0 +1,10 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.references :user, foreign_key: true
+      t.references :content, foreign_key: { to_table: 'content' }
+
+      t.timestamp
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_063619) do
+ActiveRecord::Schema.define(version: 2021_08_25_073117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2021_08_25_063619) do
     t.index ["user_id"], name: "index_content_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "content_id"
+    t.index ["content_id"], name: "index_likes_on_content_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +64,6 @@ ActiveRecord::Schema.define(version: 2021_08_25_063619) do
   end
 
   add_foreign_key "content", "users"
+  add_foreign_key "likes", "content"
+  add_foreign_key "likes", "users"
 end
