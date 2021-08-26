87acb307d5f736bf2bcca71fe42b14b7534980fd
masatora <atarashi.masatora@gmail.com>
2021-08-26 17:05:01 +0900

    refactor: Add comment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+#ALERT: CSRFの脆弱性があるのでgetリクエストは禁止すべき。ただ現状Gemが対応していないものがあるので以下のようにする。
 OmniAuth.config.allowed_request_methods = [:get, :post]
 # OmniAuth.config.silence_get_warning = true
 Rails.application.config.middleware.use OmniAuth::Builder do
