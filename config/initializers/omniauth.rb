bf7cde1c1f016625dc66312e325387be4565611b
masatora <atarashi.masatora@gmail.com>
2021-08-26 18:45:06 +0900

    GithubとGoogleログインを実装(ローカル)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,6 @@ OmniAuth.config.allowed_request_methods = [:get, :post]
 # OmniAuth.config.silence_get_warning = true
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, Rails.application.credentials.twitter[:twitter_api_key], Rails.application.credentials.twitter[:twitter_api_secret]
+  provider :github, Rails.application.credentials.github[:github_client_id], Rails.application.credentials.github[:github_client_secret]
+  provider :google_oauth2, Rails.application.credentials.google[:google_client_id], Rails.application.credentials.google[:google_client_secret]
 end
