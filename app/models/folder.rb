8e427c68b3ce80d04ff2bcb67fa1fb160377ea20
masatora <atarashi.masatora@gmail.com>
2021-09-11 23:25:16 +0900

    fix: フォルダ削除時のdependentを設定

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,7 +2,7 @@ class Folder < ApplicationRecord
   self.table_name = "folder"
 
   belongs_to :user
-  has_many :content_folder
+  has_many :content_folder, dependent: :destroy
   has_many :content, through: :content_folder
 
   validates :name, presence: true, length: { maximum: 255 }
