e276e2fca5f6c431c12789bcad48b188a05e65a9
masatora <atarashi.masatora@gmail.com>
2021-09-21 22:00:05 +0900

    会員登録時に初期コンテンツを入れておく

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :content, dependent: :destroy
   has_many :folder, dependent: :destroy
 
+  after_create :add_initial_content
+
   def self.find_for_oauth(auth)
     user = User.find_by(uid: auth.uid, provider: auth.provider)
 
@@ -24,5 +26,41 @@ class User < ApplicationRecord
   private
   def self.dummy_email(auth)
     "#{auth.uid}@#{auth.provider}.com"
+  end
+
+  private def add_initial_content
+    describe_scroll_function_content_params = {
+      title: "アプリの使い方(1) スクロール位置保存・復旧",
+      url: "https://www.youtube.com/watch?v=TzCfDE0Fd7Q&ab_channel=mgwg.pmpwtggw",
+      thumbnail_img_url: "https://img.youtube.com/vi/TzCfDE0Fd7Q/0.jpg",
+      video_playback_position: 0,
+      scroll_position_x: 0,
+      scroll_position_y: 0,
+      max_scroll_position_x: 0,
+      max_scroll_position_y: 0
+    }
+    describe_video_playback_position_function_content_params = {
+      title: "アプリの使い方(2) 動画再生位置保存・復旧",
+      url: "https://www.youtube.com/watch?v=MJdtMNIT8Qo&ab_channel=mgwg.pmpwtggw",
+      thumbnail_img_url: "https://img.youtube.com/vi/MJdtMNIT8Qo/0.jpg",
+      video_playback_position: 0,
+      scroll_position_x: 0,
+      scroll_position_y: 0,
+      max_scroll_position_x: 0,
+      max_scroll_position_y: 0
+    }
+    describe_data_migration_content_params = {
+      title: "データ移行の手順(v2以前からのユーザー様向け)",
+      url: "https://web-shiori.github.io/Shiori-iOS/Usage/data-migration",
+      thumbnail_img_url: "https://github.com/web-shiori/Shiori-chrome-extension/blob/main/doc/shiori.png?raw=true",
+      video_playback_position: 0,
+      scroll_position_x: 0,
+      scroll_position_y: 0,
+      max_scroll_position_x: 0,
+      max_scroll_position_y: 0
+    }
+    self.content.create(describe_data_migration_content_params)
+    self.content.create(describe_video_playback_position_function_content_params)
+    self.content.create(describe_scroll_function_content_params)
   end
 end
